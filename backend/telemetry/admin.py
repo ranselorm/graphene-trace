0e296e697f1d50ce70725a485e49b80083d283eb
olly01 <oliver.pulley01@gmail.com>
2026-03-04 01:19:49 +0000

    got rid of the admin as it doesnt match new model

--- backend/telemetry/admin.py
+++ backend/telemetry/admin.py
@@ -4,9 +4,4 @@ from .models import SensorFrame
 
 # Register your models here.
 
-@admin.register(SensorFrame)
-class SensorFrameAdmin(admin.ModelAdmin):
-    list_display = ("id", "patient", "timestamp", "created_at")
-    list_filter = ("patient",)
-    search_fields = ("patient__user__username", "patient__user__email")
-    ordering = ("-timestamp",)
+
