a4bdc3a2b9847e6278ba280e13e03dab48c76acc
ranselorm <ranselorm@outlook.com>
2026-03-10 11:59:00 +0000

    feat: Add useDetails hook for fetching alert details with authorization

---
+++ frontend/src/hooks/useAlertDetails.ts
@@ -0,0 +1,25 @@
+import { useAuth } from "@/context/authContext";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const API_URL = `${import.meta.env.VITE_BASE_URL}/alerts`;
+
+async function fetchDetails(token: string, alertId: string | number) {
+  const { data } = await axios.get(`${API_URL}/${alertId}/`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return data;
+}
+
+export function useDetails(alertId: string | number) {
+  const { accessToken } = useAuth();
+
+  return useQuery({
+    queryKey: ["details", alertId],
+    queryFn: () => fetchDetails(accessToken!, alertId!),
+    // only run once you have both
+    enabled: !!accessToken && !!alertId,
+    staleTime: 5 * 60 * 1000,
+  });
+}
