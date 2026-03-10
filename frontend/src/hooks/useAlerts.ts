3462704a5cf79aabd97b8ac6bed27615eaeec7c2
ranselorm <ranselorm@outlook.com>
2026-03-10 11:01:22 +0000

    feat: Implement useTransactions hook for fetching alerts with pagination

---
+++ frontend/src/hooks/useAlerts.ts
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useSelector } from "react-redux";
+// import { RootState } from "@/store/store";
+
+const API_URL = `${import.meta.env.VITE_BASE_URL}/transactions/admin`;
+
+const fetchAlerts = async (token: string, page: number, page_size: number) => {
+  const response = await axios.get(API_URL, {
+    headers: { Authorization: `Bearer ${token}` },
+    params: {
+      page,
+      page_size,
+    },
+  });
+  const alerts = response ?? [];
+  //   const count = response?.data?.data?.count ?? 0;
+  return { alerts };
+};
+
+export const useTransactions = (page: number, limit: number) => {
+  //   const token = useSelector((state: RootState) => state.user.token);
+  const token = "jjkdjdjkdkdkd";
+
+  return useQuery({
+    queryKey: ["alerts", token, page, limit],
+
+    queryFn: async () => {
+      const { alerts } = await fetchAlerts(token!, page, limit);
+      return { alerts };
+    },
+    enabled: !!token,
+  });
+};
