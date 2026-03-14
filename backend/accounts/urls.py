65d84c883f92e2c1432068d7c2cbaa0d20cc21ec
olly01 <oliver.pulley01@gmail.com>
2026-03-14 14:48:52 +0000

    insert/update api

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('me/', views.current_user, name='current_user'),
     path('get_all_users/', views.get_all_users, name='get_all_users'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('upsert_user/', views.upsert_user, name ='upsert_user'),
 ]
